95a476556e91489b7093d873daecaed314db13ae
wlandau <will.landau@gmail.com>
2021-02-14 15:26:31 -0500

    Add sessions

--- R/server.R
+++ R/server.R
@@ -1,11 +1,16 @@
 server <- function(input, output, session) {
+  session_choices <- reactiveValues(names = session_list())
   output$session_choose <- renderUI(
     pickerInput(
       inputId = "session_choose",
       label = NULL,
-      choices = session_list(),
+      choices = session_choices$names,
       selected = "main",
       multiple = FALSE
     )
   )
+  observeEvent(input$session_create, {
+    session_create(input$session_new)
+    session_choices$names <- session_list()
+  })
 }
--- R/utils_session.R
+++ R/utils_session.R
@@ -11,5 +11,26 @@ session_path <- function(session, ...) {
 }
 
 session_list <- function() {
-  list.dirs(session_user())
+  list.files(session_user())
+}
+
+session_create <- function(name) {
+  name <- trimws(name)
+  session_check_name(name)
+  session_create_impl(name)
+}
+
+session_check_name <- function(name) {
+  if (!nzchar(name)) {
+    shinyalert(title = "Error", "Please type a session name.")
+    return()
+  }
+  if (name %in% session_list()) {
+    shinyalert(title = "Error", paste("Session", name, "already exists."))
+    return()
+  }
+}
+
+session_create_impl <- function(name) {
+  dir_create(session_path(name))
 }
