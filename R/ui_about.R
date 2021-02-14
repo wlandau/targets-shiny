189f4d18051db16184d2d97be61ade19f31668f3
wlandau <will.landau@gmail.com>
2021-02-14 12:55:27 -0500

    Add about

---
+++ R/ui_about.R
@@ -0,0 +1,12 @@
+card_about <- bs4Card(
+  inputID = "about",
+  title = "About",
+  status = "primary",
+  width = 12,
+  includeMarkdown("README.md")
+)
+
+tab_about <- bs4TabItem(
+  "about",
+  card_about
+)
