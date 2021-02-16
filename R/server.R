220af11c3190d2d4713bba88d8daf157e614fe08
wlandau <will.landau@gmail.com>
2021-02-16 00:15:59 -0500

    Remove duplication and add comments

--- R/server.R
+++ R/server.R
@@ -1,33 +1,54 @@
 server <- function(input, output, session) {
+  # Important! This app changes working directories to switch projects,
+  # so we need to reset the working directory when the app exits.
+  # The proper way t odo this is with a Shiny callback.
+  # Simple on.exit() will not work.
   dir <- getwd()
   session$onSessionEnded(function() setwd(dir))
+  # Load the project the user last opened.
+  # This means reading the user's _project file
+  # to identify the project, setting the working
+  # directory there, and loading the settings.
   project_select()
   project_load()
+  # The tar_watch() module powers the "Progress" tab.
+  # This is the server-side component.
   tar_watch_server("targets-shiny")
+  # Define a special reactive to invalidate contexts
+  # when a pipeline starts or stops.
   process <- reactiveValues(running = process_running())
+  # Repeatedly check if the pipeline switched from stopped to running
+  # or vice versa.
   observe({
     invalidateLater(millis = 100)
     process$running <- process_running()
   })
+  # Toggle the shinybusy spinner whenever the pipeline switches
+  # from stopped to running or vice versa.
+  observe({
+    process$running
+    process_spinner()
+  })
+  # Every time the user selects a project in the drop-down menu
+  # of the "Control" tab, switch to that project.
   observe({
     req(input$project)
     project_set(input$project)
     project_load()
   })
+  # Every time the user tweaks the pipeline settings,
+  # update the project's files settings.rds and _targets.R
+  # The former ensures the project is recovered after a logout,
+  # and the latter defines a pipeline with the user's new settings.
   observe({
     req(input$biomarkers)
     req(input$iterations)
     project_save(input$biomarkers, input$iterations)
   })
-  observe({
-    req(input$project)
-    project_set(input$project)
-    project_load()
-  })
-  observe({
-    process$running
-    process_spinner()
-  })
+  # When the user presses the button to create a project,
+  # create a new project folder, populate it with the required files,
+  # and switch the app to the new project. Settings are copied over
+  # from the previously selected project.
   observeEvent(input$project_create, {
     req(input$project_new)
     project_create(input$project_new)
@@ -35,20 +56,29 @@ server <- function(input, output, session) {
     project_set(input$project_new)
     project_save(input$biomarkers, input$iterations)
   })
+  # When the user presses the button to delete a project,
+  # remove the current project's files and switch to the next
+  # available project. Be sure to cancel the deleted project's pipeline
+  # first.
   observeEvent(input$project_delete, {
+    process_cancel()
     project_delete(input$project)
     project_select(project_head())
   })
+  # Run the pipeline if the user presses the appropriate button.
   observeEvent(input$run_start, process_run())
+  # Stop the pipeline if the user presses the appropriate button.
   observeEvent(input$run_cancel, process_cancel())
+  # Read the latest plot output when the pipeline stops.
   output$plot <- renderPlot({
-    process$running
-    results_plot()
+    if (!process$running) results_plot()
   })
+  # Continuously refresh the stdout log file while the pipeline is running.
   output$stdout <- renderText({
     if (process$running) invalidateLater(100)
     log_text(project_stdout())
   })
+  # Continuously refresh the stdout log file while the pipeline is running.
   output$stderr <- renderText({
     if (process$running) invalidateLater(100)
     log_text(project_stderr())
