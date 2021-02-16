f03b29bfa2ed94f34284c9c159354f2b7332914e
wlandau <will.landau@gmail.com>
2021-02-16 13:49:10 -0500

    Handle empty log path

--- R/logs.R
+++ R/logs.R
@@ -1,7 +1,7 @@
 # Prep the contents of a stdout or stderr log for
 # renderText() and textOutput().
 log_text <- function(path) {
-  if (file.exists(path)) {
+  if (length(path) && file.exists(path)) {
     paste0(readLines(path), collapse = "\n")
   }
 }
