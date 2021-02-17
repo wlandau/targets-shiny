d35182b713f11d8daa485f1d1da59ce416c44032
wlandau <will.landau@gmail.com>
2021-02-17 09:49:50 -0500

    Run tests from app dir

--- tests/mode/test-transient.R
+++ tests/mode/test-transient.R
@@ -1,7 +1,6 @@
-library(rprojroot)
 library(shiny)
 Sys.unsetenv("TARGETS_SHINY_HOME")
 Sys.setenv(TARGETS_SHINY_TRANSIENT = "true")
-runApp(find_package_root_file())
+runApp()
 # Run a long pipeline, close the app, open it again, and verify
 # that the pipeline stopped running.
