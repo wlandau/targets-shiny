f2182e1a22b165128550a1df405616be8e78b666
wlandau <will.landau@gmail.com>
2021-02-17 12:57:15 -0500

    Edit

--- R/ui_control.R
+++ R/ui_control.R
@@ -8,7 +8,7 @@ card_create <- bs4Card(
     inputId = "project_new",
     label = NULL,
     value = NULL,
-    placeholder = "name of new project"
+    placeholder = "Name of new project"
   ),
   actionBttn(
     inputId = "project_create",
